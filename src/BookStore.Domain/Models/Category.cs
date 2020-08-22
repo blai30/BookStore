fc0800f22e50d7499d26b2f3ca6c3438ba0f45e4
Brian Lai <brianlai30@outlook.com>
2020-08-21 23:48:29 -0700

    Created entities and BookStoreDbContext

---
+++ src/BookStore.Domain/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BookStore.Domain.Models
+{
+    public class Category : Entity
+    {
+        public string Name { get; set; }
+
+        // Entity framework relations
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
