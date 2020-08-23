7d6bc817b62f4bdfeaf7aec677a47b04390eaeb0
Brian Lai <brianlai30@outlook.com>
2020-08-23 00:36:07 -0700

    Added DTOs, automapper, swagger, dependency injection

---
+++ src/BookStore.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Domain.Interfaces;
+using BookStore.Domain.Services;
+using BookStore.Infrastructure.Context;
+using BookStore.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookStore.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<BookStoreDbContext>()
+                .AddScoped<ICategoryRepository, CategoryRepository>()
+                .AddScoped<IBookRepository, BookRepository>()
+                .AddScoped<ICategoryService, CategoryService>()
+                .AddScoped<IBookService, BookService>();
+
+            return services;
+        }
+    }
+}
--- src/BookStore.API/Startup.cs
+++ src/BookStore.API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using BookStore.API.Configuration;
 using BookStore.Infrastructure.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace BookStore.API
 {
@@ -32,7 +35,23 @@ namespace BookStore.API
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            // Add automapper for DTO to Entity
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddControllers();
+
+            // Swagger docs
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "BookStore API",
+                    Version = "v1"
+                });
+            });
+
+            // Dependency injection extension method
+            services.ResolveDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +61,13 @@ namespace BookStore.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
 
             app.UseHttpsRedirection();
 
